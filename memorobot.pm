92405dded68b1d244c722088d04a816a059ab433
Anton Shulgin <anton.shulgin@gmx.com>
2017-03-09 01:54:21 +0300

    Added supervisors and other stuff
    - added @obey/@disobey to grant/revoke access to @commands
    - general polishing

--- memorobot.pm
+++ memorobot.pm
@@ -4,15 +4,19 @@ use strict;
 use warnings;
 
 use constant DICT_PATH => 'dict.tsv';
+use constant OBEY_PATH => 'obey.tsv';
 
 sub parse_input {
 	shift;
 	my $input = sanitize_string(shift);
 	my $sender = sanitize_string(shift);
 	if (!length($input)) {
-		return "no stuff to parse";
+		return 'No stuff to parse';
 	}
 	if (is_command($input)) {
+		if (!find_supervisor($sender)) {
+			return 'You are not my supervisor';
+		}
 		return parse_command($input);
 	}
 	return lookup($input);
@@ -20,19 +24,31 @@ sub parse_input {
 
 sub parse_command {
 	my $command_string = sanitize_string(shift);
+	if (!length($command_string)) {
+		return 'No';
+	}
 	my ($command, $params) = ($command_string =~ m/^@([^\s]+)\s+(.+)$/);
-	print("performing '$command' with params '$params'...\n");
-	if ($command =~ m/^add$/i) {
+	# TODO: do the switch or a hash map
+	if ($command eq 'add') {
 		return add_memo($params);
 	}
-	if ($command =~ m/^remove$/i) {
+	if ($command eq 'remove') {
 		return remove_memo($params);
 	}
-	return "No";
+	if ($command eq 'obey') {
+		return add_supervisor($params);
+	}
+	if ($command eq 'disobey') {
+		return remove_supervisor($params);
+	}
+	return 'No comprendo, amigo';
 }
 
 sub lookup {
 	my $term = sanitize_string(shift);
+	if (!length($term)) {
+		return 'Usage: <my nickname> <term>';
+	}
 	my $memo = find_memo($term);
 	if (!$memo) {
 		return "No such thing as '$term'";
@@ -42,13 +58,9 @@ sub lookup {
 
 sub remove_memo {
 	my $term = sanitize_string(shift);
-	if (!length($term)) {
-		return "no stuff to look up";
-	}
 	if (!find_memo($term)) {
 		return "No such thing as '$term'";
 	}
-	print("looking up '$term'...\n");
 	my @memos = get_memos();
 	my @new_memos;
 	for my $memo (@memos) {
@@ -65,7 +77,6 @@ sub remove_memo {
 sub add_memo {
 	my $params = sanitize_string(shift);
 	my ($term, $text) = ($params =~ m/^([^\s]+)\s+(.+)$/i);
-	print("adding '$term' as '$text'...\n");
 	if (find_memo($term)) {
 		return "'$term' already exists";
 	}
@@ -78,9 +89,8 @@ sub add_memo {
 sub find_memo {
 	my $term = sanitize_string(shift);
 	if (!length($term)) {
-		return "no stuff to look up";
+		return 'Usage: <my nickname> <word>';
 	}
-	print("looking up '$term'...\n");
 	my @memos = get_memos();
 	for my $memo (@memos) {
 		if ($memo =~ m/^${term}\t(.+)$/i) {
@@ -98,6 +108,60 @@ sub get_memos {
 	return @memos;
 }
 
+sub remove_supervisor {
+	my $nickname = sanitize_string(shift);
+	if (!find_supervisor($nickname)) {
+		return "$nickname is not my supervisor";
+	}
+	my @supervisors = get_supervisors();
+	my @new_supervisors;
+	for my $supervisor (@supervisors) {
+		if ($nickname ne sanitize_string($supervisor)) {
+			push(@new_supervisors, $supervisor);
+		}
+	}
+	open(OBEY_FILE, '>', OBEY_PATH);
+	print OBEY_FILE @new_supervisors;
+	close(OBEY_FILE);
+	return "$nickname is no longer my supervisor";
+}
+
+sub add_supervisor {
+	my $nickname = sanitize_string(shift);
+	if (!length($nickname)) {
+		return 'Can\'t obey nobody';
+	}
+	if (find_supervisor($nickname)) {
+		return "$nickname is already my supervisor";
+	}
+	open(OBEY_FILE, '>>', OBEY_PATH);
+	print OBEY_FILE "$nickname\n";
+	close(OBEY_FILE);
+	return "$nickname is now my supervisor";
+}
+
+sub find_supervisor {
+	my $nickname = sanitize_string(shift);
+	if (!length($nickname)) {
+		return;
+	}
+	my @supervisors = get_supervisors();
+	for my $supervisor (@supervisors) {
+		if ($nickname eq sanitize_string($supervisor)) {
+			return $supervisor;
+		}
+	}
+	return;
+}
+
+sub get_supervisors {
+	my @supervisors;
+	open(OBEY_FILE, '<', OBEY_PATH);
+	@supervisors = <OBEY_FILE>;
+	close(OBEY_FILE);
+	return @supervisors;
+}
+
 sub is_command {
 	my $query = shift;
 	return ($query =~ m/^@\w+/);
@@ -105,7 +169,7 @@ sub is_command {
 
 sub sanitize_string {
 	my $string = shift;
-	$string =~ s/^\s+|\s+$//;
+	$string =~ s/^\s+|\n|\r|\s+$//;
 	$string =~ s/\s{2,}//gi;
 	return $string;
 }
